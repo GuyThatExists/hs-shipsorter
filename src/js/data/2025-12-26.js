648727f7c66d3e68ce3e46b66fb82ea16f376da6
GuyThatExists <147623285+GuyThatExists@users.noreply.github.com>
2025-12-27 03:36:56 -0500

    Update 2025-12-26.js

--- src/js/data/2025-12-26.js
+++ src/js/data/2025-12-26.js
@@ -13,456 +13,532 @@ dataSet[dataSetVersion].options = [
 dataSet[dataSetVersion].characterData = [
   {
     name: "Grimdorks (June x Rose)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Rosemary (Rose x Kanaya)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "DaveJade",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "RoseJade",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Pepsicola (June x Dave)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Arasol (Aradia x Sollux)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Erisol (Eridan x Sollux)",
+    img: "shiplist pics.png",
     opts: {
       eridan: true
     }
   },
   {
     name: "Roxygen (Roxy x June)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Grimdorks (John x Rose)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Pepsicola (John x Dave)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Roxygen (Roxy x John)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "CallieRoxy",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "DirkJake",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "JaneJake",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Davekat (Dave x Karkat)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Gamkat (Gamzee x Karkat)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Nepkat (Nepeta x Karkat)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Spider8reath (Vriska x John)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Spider8reath (Vriska x June)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Vrisrezi (Vriska x Terezi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Roserezi (Rose x Terezi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Johnrezi (John x Terezi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Junerezi (June x Terezi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Gamtav (Gamzee x Tavros)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Jadenep (Jade x Nepeta)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "DirkRoxy",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Cotton Candy (Roxy x Jane)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Javepeta (Jade x Davepeta)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Aldirk (Alt. Callie x Dirk)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Karkan (Karkat x Kanaya)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Dirkuu (Dirk x Caliborn)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Jakeuu (Jake x Caliborn)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Gamjane (Gamzee x Jane)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Jakekat (Jake x Karkat)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Vristav (Vriska x Tavros)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Solfef (Sollux x Feferi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Araequius (Aradia x Equius)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Meowrails (Nepeta x Equius)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Snowjack (Snowman x Spades Slick)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "PMJack (PM x Bec Noir)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "WVPM (Mayor x PM)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "DocJack (Doc Scratch x Spades Slick)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "HussDoc (Hussie x Doc Scratch)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Arameen (Aradia x Meenah)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Cronkri (Cronus x Kankri)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Meenkat (Meenah x Karkat)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "JakeRoxy",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Vrisuius (Vriska x Equius)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Johnuu (John x Caliborn)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Juneuu (June x Caliborn)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Sharknade (Eridan x John)",
+    img: "shiplist pics.png",
     opts: {
       eridan: true
     }
   },
   {
     name: "MeenJohn (Meenah x John)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "MeenJune (Meenah x June)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Johnvrisrezi (John x Vriska x Terezi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Junevrisrezi (June x Vriska x Terezi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Aranea x Cronus",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Aranea x Porrim",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Aranea x Damara",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Meulin x Kurloz",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Meulin x Damara",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Mituna x Latula",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Vriska x Damara",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Cronuscest (Cronus x Cronus)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Swagger (Swiss x Dagger)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Vristavvy (Vrissy x Tavvy)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Swisstavvy (Swiss x Tavvy)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Dagger x CT/Tanto",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Scratch n Sniff (Nepeta x Terezi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Aravris (Aradia x Vriska)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Rosefef (Rose x Feferi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Jadekan (Jade x Kanaya)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "DirkJohn",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "DirkJune",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "Jadefef (Jade x Feferi)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "JadeRose",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
   },
   {
     name: "White Bread (Swiss x Harry)",
+    img: "shiplist pics.png",
     opts: {
       eridan: false
     }
